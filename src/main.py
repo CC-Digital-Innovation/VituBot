c39c3267e8efc072ee21b5a5265d86b430172fce
Anthony Farina <farinaanthony96@gmail.com>
2025-03-07 18:01:42 -0500

    attempt to point uvicorn to "app" declaration named "vitubot" in main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run('vitubot:VITUBOT_APP', host='0.0.0.0', port = 8000)
+    uvicorn.run('src.vitubot:VITUBOT_APP', host='0.0.0.0', port = 8000)
